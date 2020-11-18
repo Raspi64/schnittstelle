3209fab19d14b2862a30e112b12eff5ada49525d
Simon Struck <simon@simonscode.org>
2020-11-18 19:19:59 +0100

    Fix error constant

--- src/plugins/basic/basic_function.cpp
+++ src/plugins/basic/basic_function.cpp
@@ -1,21 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include "basic_function.h"
 
 
 namespace basic_plugin {
     int basic_fabian(struct mb_interpreter_t *bas, void **ptr) {
         printf("Fabian ist der BESTE\n");
-        return 0;
+        return MB_FUNC_OK;
     }
 
     int basic_openTerminal(struct mb_interpreter_t *bas, void **ptr) {
         system("xfce4-terminal");
-        return 0;
+        return MB_FUNC_OK;
     }
 
     int basic_echo(struct mb_interpreter_t *bas, void **ptr) {
         printf("Echo\n");
-        return 0;
+        return MB_FUNC_OK;
     }
 }
